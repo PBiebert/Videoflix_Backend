073ecacc2fe67db3c5946536c7debef2ac484018
Philipp Biebert <philipp.biebert@gmail.com>
2026-07-23 15:30:08 +0200

    feat(accounts): Refactor email templates to use a base layout and include the logo. translate emails into English.

--- accounts/api/services.py
+++ accounts/api/services.py
@@ -3,28 +3,26 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
+LOGO_URL = "https://philippbiebert.de/assets/img/Logo_Videoflix.png"
+
 
 def send_activation_email(user, activation_link):
-    # Kontext-Variablen für das Template definieren
-    context = {"user": user, "activation_link": activation_link}
+    context = {"user": user, "activation_link": activation_link, "logo_url": LOGO_URL}
 
-    # Template rendern und Plain-Text aus dem HTML erzeugen
     html_content = render_to_string("accounts/activation_email.html", context)
     text_content = strip_tags(html_content)
 
-    # E-Mail Objekt erstellen
-    subject = "Aktiviere deinen Videoflix-Account"
+    subject = "Activate your Videoflix account"
     from_email = settings.DEFAULT_FROM_EMAIL
     to = [user.email]
     msg = EmailMultiAlternatives(subject, text_content, from_email, to)
     msg.attach_alternative(html_content, "text/html")
 
-    # E-Mail versenden
     msg.send()
 
 
 def send_password_reset_email(user, reset_link):
-    context = {"user": user, "reset_link": reset_link}
+    context = {"user": user, "reset_link": reset_link, "logo_url": LOGO_URL}
 
     html_content = render_to_string("accounts/reset_password_email.html", context)
     text_content = strip_tags(html_content)
