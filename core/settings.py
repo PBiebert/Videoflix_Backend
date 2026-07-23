bdc93cbcc531e345e07cf15ae9af5f5c75747dfe
Philipp Biebert <philipp.biebert@gmail.com>
2026-07-23 16:53:07 +0200

    feat(video_library): add initial video library app structure with basic files

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "django_rq",
     "accounts.apps.AccountsConfig",
+    "video_library.apps.VideoLibraryConfig",
 ]
 
 MIDDLEWARE = [
