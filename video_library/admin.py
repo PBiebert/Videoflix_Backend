1d70134c1c0836b91d469b356d828081a19233de
Philipp Biebert <philipp.biebert@gmail.com>
2026-07-28 13:02:30 +0200

    feat(video_library): register Video model with custom admin interface

--- video_library/admin.py
+++ video_library/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from video_library.models import Video
 
-admin.site.register(Video)
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "category"]
